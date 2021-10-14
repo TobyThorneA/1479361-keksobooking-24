28d17c9f9d51e22355dae88c84d9681a4fdefad9
TobyThorneA <igraman_XX@mail.ru>
2021-10-14 18:58:21 +0300

    Сделал модуляцию

---
+++ js/util.js
@@ -0,0 +1,30 @@
+function getRandomInteger (smaller, larger){
+  const lower = Math.ceil(Math.min(Math.abs(smaller), Math.abs(larger)));
+  const upper = Math.floor(Math.max(Math.abs(smaller), Math.abs(larger)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+export{getRandomInteger};
+function getRandomFraction (smaller, larger, after = 1){
+  const lower = Math.min(Math.abs(smaller), Math.abs(larger));
+  const upper = Math.max(Math.abs(smaller), Math.abs(larger));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(after);
+}
+export{getRandomFraction};
+// Функцию getArray взял с сайта : https://ru.stackoverflow.com/questions/1293985/Как-создать-массив-строк-из-случайной-длины-и-случайных-значений
+// Не очень хорошо понял как она работает.
+function getArray(randomLength) {
+  const maxLength = randomLength.length;
+  const lengthOfArray = getRandomInteger(1, maxLength);
+  const array = [];
+  for(let index = 0; index < lengthOfArray; index++) {
+    const indexOfEl = getRandomInteger(0, 5);
+    const el = randomLength[indexOfEl];
+    if (!array.includes(el)) {
+      array.push(el);
+    }
+  }
+  return array;
+}
+export {getArray};
